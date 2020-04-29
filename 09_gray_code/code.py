8d6f45dafa5f49ab6ea4549be826777eced7e4ad
ki-ARA-sh <kiarash.ara@gmail.com>
2020-04-29 23:27:14 +0430

    Till the end of gray code

---
+++ 09_gray_code/code.py
@@ -0,0 +1,20 @@
+
+def gray_code(length):
+    if length == 1:
+        return ["0", "1"]
+    a = gray_code(length - 1)
+    result = list()
+    alternate = False
+    for i in range(len(a)):
+        result.append(str(int(alternate)) + a[i])
+        result.append(str(int(not alternate)) + a[i])
+        alternate = not alternate
+    return result
+
+
+def main():
+    print(', '.join(gray_code(int(input()))))
+
+
+if __name__ == "__main__":
+    main()
