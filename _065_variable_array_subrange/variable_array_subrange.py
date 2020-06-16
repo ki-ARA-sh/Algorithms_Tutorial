73c6907f16ae637d79321a79291f8deb4e1323fd
ki-ARA-sh <kiarash.ara@gmail.com>
2020-06-16 18:22:32 +0430

    Till the end of variable array subrange

--- _065_variable_array_subrange/variable_array_subrange.py
+++ _065_variable_array_subrange/variable_array_subrange.py
@@ -1,20 +1,40 @@
+import bisect
+from math import sqrt
+
+# bisect.insort()
+def get_ps(n, a):
+    ps = [0 for i in range(n + 1)]
+    for i in range(1, n + 1):
+        ps[i] = ps[i - 1] + a[i - 1]
+    return ps
+
 
 def main():
     n, q = map(int, input().split())
     a = list(map(int, input().split()))
-    ps = [0 for i in range(n)]
-    ps[0] = a[0]
-    for i in range(1, n):
-        ps[i] = ps[i - 1] + a[i]
+    # ps = get_ps(n, a)
+    changes = []
+    t = int(sqrt(n))
     for i in range(q):
+        if i % t == 0:
+            changes.clear()
+            ps = get_ps(n, a)
         row = list(map(int, input().split()))
         if row[0] == 1:
             l = row[1]
             r = row[2]
+            index = bisect.bisect(changes, (l, float('-inf')))
+            ans = ps[r + 1] - ps[l]
+            while index < len(changes) and changes[index][0] <= r:
+                ans += changes[index][1]
+                index += 1
+            print(ans)
         elif row[0] == 2:
             idx = row[1]
             nv = row[2]
-        pass
+            bisect.insort(changes, (idx, nv - a[idx]))
+            a[idx] = nv
+    pass
 
 
 if __name__ == '__main__':
